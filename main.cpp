6ecea0932f2d6cb1fab3e758e6835e3edd8d362c
JosiahBowe <>
2021-09-30 02:37:21 +0000

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+int main(){
+string noun1,verb1,verb2,adjective1,time1;
+cout<<"Enter a noun:";
+cin>>noun1;
+cout<<"Enter a verb:";
+cin>>verb1;
+cout<<"Enter a verb:";
+cin>>verb2;
+cout<<"Enter an adjective:";
+cin>>adjective1;
+cout<<"Enter a time:";
+cin>>time1;
+cout<<noun1<< " "  <<"Walked outside to the grill"<<endl;
+cout<<"You"<<" "<<verb1<<" "<<"Back home"<<endl;
+cout<<"After that, you "<<verb2<<" "<<"on your sister"<<endl;
+cout<<"Your"<<" "<<adjective1<<" "<<"Sister went to bed"<<endl;
+cout<<"It was"<<" "<<time1<<" "<<"O'clock";
+
+
+
+
+
+
+return 0;
+}
