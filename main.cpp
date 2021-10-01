f318251d9d14b3af244127d13add500c1b97e7c6
Siah2021 <91646062+Siah2021@users.noreply.github.com>
2021-09-30 17:11:25 -1000

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,19 +2,19 @@
 using namespace std;
 int main(){
 string noun1,verb1,verb2,adjective1,time1;
-cout<<"Enter a noun:";
+cout<<"Enter a noun(poper noun):";
 cin>>noun1;
-cout<<"Enter a verb:";
+cout<<"Enter a verb:(past tense)";
 cin>>verb1;
-cout<<"Enter a verb:";
+cout<<"Enter a verb(past tense:";
 cin>>verb2;
-cout<<"Enter an adjective:";
+cout<<"Enter an adjective(that descrides your noun):";
 cin>>adjective1;
-cout<<"Enter a time:";
+cout<<"Enter a time(in word form):";
 cin>>time1;
-cout<<noun1<< " "  <<"Walked outside to the grill"<<endl;
-cout<<"You"<<" "<<verb1<<" "<<"Back home"<<endl;
-cout<<"After that, you "<<verb2<<" "<<"on your sister"<<endl;
+cout<<noun1<< " "  <<"walked outside to the grill"<<endl;
+cout<<"You"<<" "<<verb1<<" "<<"back home"<<endl;
+cout<<"After that, you"<<" "<<verb2<<" "<<"on your sister"<<endl;
 cout<<"Your"<<" "<<adjective1<<" "<<"Sister went to bed"<<endl;
 cout<<"It was"<<" "<<time1<<" "<<"O'clock";
 
